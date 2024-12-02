c5ca88f8fb6502743e1d5efb7df9ac82d0f90bd5
SuvaSharma <sharmasuva1@gmail.com>
2024-12-02 12:45:50 +0545

    minor changes

--- mindgames/lib/Domains.dart
+++ mindgames/lib/Domains.dart
@@ -94,61 +94,79 @@ class _DomainPageState extends State<DomainPage> {
                 padding: EdgeInsets.all(padding),
                 child: Column(
                   children: [
-                    GestureDetector(
-                      onTap: () async {
-                        // Access shared preferences to check if parental lock is enabled
-                        final SharedPreferences prefs =
-                            await SharedPreferences.getInstance();
-                        final bool parentalLockEnabled =
-                            prefs.getBool('parental_lock_enabled') ?? true;
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: AnimatedButton(
+                        onPressed: () async {
+                          // Access shared preferences to check if parental lock is enabled
+                          final SharedPreferences prefs =
+                              await SharedPreferences.getInstance();
+                          final bool parentalLockEnabled =
+                              prefs.getBool('parental_lock_enabled') ?? true;
 
-                        if (parentalLockEnabled) {
-                          if (!context.mounted) return;
+                          if (parentalLockEnabled) {
+                            if (!context.mounted) return;
 
-                          // Fetch the signed-in user (similar to MainPage logic)
-                          final user = await getCurrentUser();
+                            // Fetch the signed-in user (similar to MainPage logic)
+                            final user = await getCurrentUser();
 
-                          // Show PIN verification dialog and navigate to MainWrapper on success
-                          showPinVerificationDialog(
-                              context, user, const MainWrapper());
-                        } else {
-                          // If parental lock is disabled, navigate to MainWrapper directly
-                          if (!context.mounted) return;
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const MainWrapper()),
-                          );
-                        }
-                      },
-                      child: Align(
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          height: baseSize * 0.13,
-                          width: baseSize * 0.42,
-                          decoration: BoxDecoration(
-                            color: Color(0xFF309092),
-                            // border: Border.all(width: 1, color: Colors.black),
-                            borderRadius:
-                                BorderRadius.circular(baseSize * 0.03),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.2),
-                                blurRadius: 6,
-                                offset: Offset(0, 4),
-                              ),
-                            ],
-                          ),
-                          child: Center(
-                            child: Text(
-                              "Parents' Corner".tr,
-                              style: TextStyle(
-                                fontSize: baseSize * 0.05,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white,
+                            // Show PIN verification dialog and navigate to MainWrapper on success
+                            showPinVerificationDialog(
+                                context, user, const MainWrapper());
+                          } else {
+                            // If parental lock is disabled, navigate to MainWrapper directly
+                            if (!context.mounted) return;
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MainWrapper()),
+                            );
+                          }
+                        },
+                        color: const Color.fromARGB(
+                            255, 69, 149, 189), // Button color
+                        height: baseSize * 0.17, // Button height
+                        width: baseSize * 0.41, // Button width
+                        shadowDegree: ShadowDegree.dark, // Shadow intensity
+                        shape: BoxShape.rectangle, // Button shape
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // CircleAvatar with icon inside
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15.0),
+                              child: CircleAvatar(
+                                radius: baseSize *
+                                    0.04, // Adjust the size as needed
+                                backgroundColor: Colors.white,
+                                child: Icon(
+                                  Icons.people, // People icon
+                                  color: const Color(0xFF309092), // Icon color
+                                  size: baseSize * 0.07, // Icon size
+                                ),
                               ),
                             ),
-                          ),
+                            SizedBox(
+                                width: baseSize *
+                                    0.04), // Space between icon and text
+                            // Wrapping the text in a Flexible widget
+                            Flexible(
+                              child: Text(
+                                "Parent Corner".tr,
+                                textAlign: TextAlign
+                                    .center, // Center-align the multi-line text
+                                maxLines:
+                                    2, // Allow text to wrap into at most two lines
+                                overflow: TextOverflow
+                                    .visible, // Ensure text doesn't overflow
+                                style: TextStyle(
+                                  fontSize: baseSize * 0.05,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
